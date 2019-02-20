14ac5cbf0b22a3307db6a8d8310d3f22ac497dbb
Naelle Zephir <nzephir@bu.edu>
2019-02-20 17:52:35 -0500

    updated videos

--- ffmpeg.py
+++ ffmpeg.py
@@ -3,30 +3,33 @@ import threading
 import subprocess
 import os
 
-q = queue.Queue()
+
 path = '/Users/naellezephir/Desktop/GitHub/exercise-2/videos'
 outpath = '/Users/naellezephir/Desktop/GitHub/exercise-2/videos/finished'
 
-def fill_q():
+def fill_q(Q):
 	folder = os.listdir(path)
 	for file in folder:
-		q.put(file)
+		Q.put(file)
 
-def ffmpeg_convert(q):
-	while not q.empty():
-		curr_file = q.get()
+def ffmpeg_convert(Q):
+	while True:
+		curr_file = Q.get()
 		vid720 = "ffmpeg -i "+path+"/"+curr_file+" -s hd720 -b:v 1M -r 30 "+outpath+"/"+curr_file[:-4]+"720.mp4"
 		vid480 = "ffmpeg -i "+path+"/"+curr_file+" -s hd720 -b:v 1M -r 30 "+outpath+"/"+curr_file[:-4]+"480.mp4"
+		print(curr_file + " 720 is processing")
 		subprocess.call(vid720, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
 		print(curr_file + " 720 is finished")
+		print(curr_file + " 480 is processing")
 		subprocess.call(vid480, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT, shell=True)
 		print(curr_file + " 480 is finished")
 
 
 def main():
-	fill_q()
+	q = queue.Queue()
+	fill_q(q)
 	for i in range(2):
-		worker = threading.Thread(target=ffmpeg_convert, args=(q))
+		worker = threading.Thread(target=ffmpeg_convert, args=(q,))
 		worker.start()
 
 if __name__ == "__main__":
