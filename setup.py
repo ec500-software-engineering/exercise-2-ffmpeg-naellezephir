c6563a049572a6a635bd4dac90b38ada68bdde7f
Naelle Zephir <nzephir@bu.edu>
2019-02-23 13:14:01 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
 	name='exercise-2-ffmpeg-naellezephir',
-    version='',
-    description='async ffmpeg exercise',
-    author='Naelle Zephir',
-    author_email='nzephir@bu.edu',
-    url='',
-    packages=[],
+	version='',
+	description='async ffmpeg exercise',
+	author='Naelle Zephir',
+	author_email='nzephir@bu.edu',
+	url='',
+	packages=[],
 )
