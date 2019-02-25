299a2a286036acaaa9ce0adf240c3de06d0bfdb0
Naelle Zephir <nzephir@bu.edu>
2019-02-25 17:27:06 -0500

    updated unit test

--- test_ffmpeg.py
+++ test_ffmpeg.py
@@ -1,13 +1,15 @@
 from ffmpeg import ffmpeg_convert
 import queue 
 import pytest
+import duration
 
 def test_ffmpeg(genpat):
 	q = queue.Queue()
 	vid = str(genpat)
 	q.put(vid)
+	original_duration = duration.find_dur(vid)
 	dur = ffmpeg_convert(q,vid)
-	assert dur == pytest.approx(1.)
+	assert original_duration == dur
 
 if __name__ == '__main__':
 	pytest.main(['-x',__file__])
